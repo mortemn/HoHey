79f991889bc1c123cd1732e0e54e0c9dacedb352
mortem <lampakyu@gmail.com>
2021-07-01 01:54:03 +0800

    finally fixed all the errors

--- contracts/MaiGeneration.sol
+++ contracts/MaiGeneration.sol
@@ -7,11 +7,12 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Mai is ERC20, ERC20Burnable, AccessControl {
+    using SafeMath for uint256;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant VOTER_ROLE = keccak256("VOTER_ROLE");
 
     event _VoteMade(uint256 indexed pollID, uint256 votes, address indexed voter);
-    event _PollCreated(uint voteQuorum, uint commitEndDate, uint revealEndDate, uint indexed pollID, address indexed creator);
+    event _PollCreated(uint voteQuorum, uint commitEndDate, uint indexed pollID, address indexed creator);
     event _VotingRightsGranted(address indexed voter);
     event _VotingRightsRevoked(address indexed voter);
 
@@ -27,7 +28,7 @@ contract Mai is ERC20, ERC20Burnable, AccessControl {
         uint256 voteQuorum;
         uint256 votesFor;
         uint256 votesAgainst;
-        mapping(address => uint256) _votesCommitted;
+        mapping(address => uint256) _votesClaimed;
         mapping(address => uint256) _votes;
     }
 
@@ -35,14 +36,13 @@ contract Mai is ERC20, ERC20Burnable, AccessControl {
     uint256 pollNonce = 0;
 
     function startPoll(uint256 _voteQuorum, uint256 _commitDuration) public returns (uint256 pollID) {
-        uint commitEndDate = block.timestamp.add(_commitDuration);
+        uint256 commitEndDate = block.timestamp.add(_commitDuration);
         pollNonce = pollNonce.add(1);
-        pollMapping[pollNonce] = Poll({
-            voteQuorum: _voteQuorum,
-            commitEndDate: commitEndDate,
-            votesFor: 0,
-            votesAgainst: 0
-        });
+        Poll storage newPoll = pollMapping[pollNonce];
+        newPoll.voteQuorum = _voteQuorum;
+        newPoll.commitEndDate = commitEndDate;
+        newPoll.votesFor = 0;
+        newPoll.votesAgainst = 0;
 
         emit _PollCreated(_voteQuorum, commitEndDate, pollNonce, msg.sender);
         return pollNonce;
@@ -60,31 +60,32 @@ contract Mai is ERC20, ERC20Burnable, AccessControl {
     }
 
     modifier voterCheck(address voter) {
-        if (hasRole(VOTER_ROLE) == False && numOfTokens >= 1000) {
+        if (hasRole(VOTER_ROLE, voter) == false && balanceOf(voter) >= 1000) {
             voterAllocation(voter);
         }
-        if (hasRole(VOTER_ROLE) == True && numOfTokens < 1000) {
+        if (hasRole(VOTER_ROLE, voter) == true && balanceOf(voter) < 1000) {
            voteRevokation(voter); 
         }
-        require(hasRole(VOTER_ROLE) || balanceOf(voter) >= 1000, "Only addresses that have balances of over 1000 tokens are able to vote");
+        require(hasRole(VOTER_ROLE, voter) || balanceOf(voter) >= 1000, "Only addresses that have balances of over 1000 tokens are able to vote");
+        _;
     }
 
-    function claimVote(address voter) public voterCheck {
-        uint256 claimable = sub(_votesClaimed[voter], 999);
+    function claimVote(address voter, uint256 pollID) public voterCheck(voter) {
+        uint256 claimable = pollMapping[pollID]._votesClaimed[voter].sub(999);
         require(claimable > 0, "Address has no votes available to claim");
-        pollMapping[pollID]._votesClaimed[voter] = add(_votesClaimed[voter], claimable);
-        pollMapping[pollID]._votes[voter] = add(_votes[voter], claimable);
+        pollMapping[pollID]._votesClaimed[voter] = pollMapping[pollID]._votesClaimed[voter].add(claimable);
+        pollMapping[pollID]._votes[voter] = pollMapping[pollID]._votes[voter].add(claimable);
     }
 
-    function numOfVotes(address voter, uint256 pollID) public returns (uint256 votes) {
+    function numOfVotes(address voter, uint256 pollID) public view returns (uint256 votes) {
         return pollMapping[pollID]._votes[voter]; 
     }
 
-    function makeVote(address voter, uint256 votesMade, uint256 pollID) public voterCheck {
-        require(isNotExpired);
+    function makeVote(address voter, uint256 votesMade, uint256 pollID) public voterCheck(voter) {
+        require(isNotExpired(pollID));
         require(pollMapping[pollID]._votes[voter] > 0, "Address has no votes currently, call function claimVote to claim a vote");
-        require(votesMade <= _pollMapping[pollID].votes[voter]);
-        pollMapping[pollID]._votes[voter] = sub(_pollMapping[pollID].votes[voter], votesMade);
+        require(votesMade <= pollMapping[pollID]._votes[voter]);
+        pollMapping[pollID]._votes[voter] = pollMapping[pollID]._votes[voter].sub(votesMade);
         emit _VoteMade(pollID, votesMade, voter); 
     }
 
@@ -93,17 +94,17 @@ contract Mai is ERC20, ERC20Burnable, AccessControl {
         _mint(to, amount);
     }
 
-    function pollExists(uint256 _pollID) public returns (bool exists) {
+    function pollExists(uint256 _pollID) public view returns (bool exists) {
         return (_pollID != 0 && _pollID <= pollNonce);
     }
 
-    function pollEnded(uint256 _pollID) public returns (bool ended) {
+    function pollEnded(uint256 _pollID) public view returns (bool ended) {
         require(pollExists(_pollID));
 
-        return isExpired(pollMapping[_pollID].commitEndDate);
+        return isNotExpired(pollMapping[_pollID].commitEndDate);
     }
 
-    function isExpired(uint256 _terminationDate) public returns (bool expired) {
+    function isNotExpired(uint256 _terminationDate) public view returns (bool expired) {
         return (block.timestamp > _terminationDate);
     }
 
