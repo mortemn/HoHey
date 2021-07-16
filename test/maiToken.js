60a07c19b1db9cad602ec5180f9edfbd320d340f
mortem <lampakyu@gmail.com>
2021-07-16 16:18:42 +0800

    working

--- test/maiToken.js
+++ test/maiToken.js
@@ -1,16 +1,15 @@
 const MaiContract = artifacts.require("Mai");
 
 contract("MaiToken", () => {
-  var tokenPrice = 1000000000;
-  var tokensAvailable = 750000;
   it("Should deploy smart contract properly", async () => {
-    const maiToken = MaiContract.deployed();
-      .then(instance => instance.getBalance.call(accounts[0])
-      .then(balance => {
+    MaiContract.deployed()
+      .then((instance) => instance.getBalance.call(accounts[0]))
+      .then((balance) => {
         assert.equal(
           balance.valueOf(),
           10000,
           "10000 wasn't in the first account"
         );
+      });
   });
 });
