16f4bd42374bd45a266ab162e67dd70e64b84caf
mortem <lampakyu@gmail.com>
2021-07-19 16:05:17 +0800

    with promise

--- test/maiToken.js
+++ test/maiToken.js
@@ -1,7 +1,17 @@
 const MaiContract = artifacts.require("Mai");
 
-contract("MaiToken", () => {
-  it("Should deploy smart contract properly", async () => {
+contract("MaiToken", async (accounts) => {
+  it("Should deploy token", async () => {
+    const instance = await MaiContract.deployed();
+    console.log(instance.address);
+    assert(instance !== "");
+  });
+  it("Should put 10000 Mai tokens in first account with aync await", async () => {
+    const instance = await MaiContract.deployed();
+    const balance = await instance.getBalance.call(accounts[0]);
+    assert.equal(balance.valueOf(), 100000, "10000 wasn't in first account");
+  });
+  it("Should put 10000 Mai tokens in first account with promises", async () => {
     MaiContract.deployed()
       .then((instance) => instance.getBalance.call(accounts[0]))
       .then((balance) => {
